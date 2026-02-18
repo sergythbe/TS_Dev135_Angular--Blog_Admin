6f7c1bc7aa9daf3e38aefac8eeb86ba547545abb
sani serge <sergyth@gmail.com>
2026-02-18 14:58:37 +0100

    ajout des interfaces post et user puis du services postServices

--- blog-admin/src/app/core/services/post.service.ts
+++ blog-admin/src/app/core/services/post.service.ts
@@ -1,8 +1,18 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Post } from '@models/post.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PostService {
-  
+  private http = inject(HttpClient);
+  private apiUrl = 'https://jsonplaceholder.typicode.com/posts';
+
+  getPosts(): Observable<Post[]>{
+    return this.http.get<Post[]>(this.apiUrl)
+  }
+
+
 }
